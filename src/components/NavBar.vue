d420e5c6026351335d545d0e476f954c2b82a27a
radualexandru1986 <radu.c.alexandru@gmail.com>
2022-03-17 19:42:26 +0000

    added popperJs.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,13 @@
 <template>
     <nav class="navbar">
-        <a class="navbar-brand text-light"> Contact Form</a>
+        <div class="container">
+            <a class="navbar-brand text-light"> Contact Form</a>
         <button class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Form</button>
         <form class="form-inline">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn  my-2 my-sm-0 " type="button">Search</button>
          </form>
+        </div>
     </nav>
 </template>
 
